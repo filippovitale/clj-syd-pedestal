9c03216076fdc4d95ccf882a1e61937b7505b724
Filippo Vitale <filippo.vitale@gmail.com>
2013-08-29 20:20:05 +1000

    changed the renderings

--- app/src/hw/rendering.cljs
+++ app/src/hw/rendering.cljs
@@ -1,22 +1,20 @@
 (ns hw.rendering
   (:require [domina :as dom]
             [io.pedestal.app.render.push :as render]
+            [io.pedestal.app.render.events :as events]
+            [io.pedestal.app.messages :as msg]
             [io.pedestal.app.render.push.templates :as templates]
             [io.pedestal.app.render.push.handlers.automatic :as d])
   (:require-macros [hw.html-templates :as html-templates]))
 
-(def templates (html-templates/hw-templates))
-
-(defn render-page [renderer [_ path] transmitter]
-  (let [parent (render/get-parent-id renderer path)
-        id (render/new-id! renderer path)
-        html (templates/add-template renderer path (:hw-page templates))]
-    (dom/append! (dom/by-id parent) (html {:id id :message ""}))))
-
-(defn render-message [renderer [_ path _ new-value] transmitter]
-  (templates/update-t renderer path {:message new-value}))
+(defn button-enable [r [_ path transform-name messages] d]
+  (events/send-on-click (dom/by-id "msg-button")
+                          d
+                          transform-name
+                          [{msg/type :set-value msg/topic [:greeting] :value "Pedestal Rocks!"}]))
 
 (defn render-config []
   [[:node-create  [:greeting] render-page]
    [:node-destroy   [:greeting] d/default-exit]
-   [:value [:greeting] render-message]])
+   [:value [:greeting] render-message]
+   [:transform-enable [:greeting] button-enable]])
