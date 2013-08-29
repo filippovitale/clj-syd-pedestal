b241b5ff96c75a1feb183a7eab290a5069f55e0c
Filippo Vitale <filippo.vitale@gmail.com>
2013-08-29 20:01:45 +1000

    added button and changed behaviour

--- app/src/hw/behavior.clj
+++ app/src/hw/behavior.clj
@@ -1,11 +1,14 @@
 (ns ^:shared hw.behavior
-    (:require [clojure.string :as string]
-              [io.pedestal.app.messages :as msg]))
+  (:require [clojure.string :as string]
+            [io.pedestal.app :as app]
+            [io.pedestal.app.messages :as msg]))
 
-(defn set-value-transform [old-value message]
-  (:value message))
+(defn init-main [_]
+  [[:transform-enable [:greeting ] :set-value [{msg/topic :set-value}]]])
 
 (def example-app
   {:version 2
-   :transform [[:set-value [:greeting] set-value-transform]]})
-
+   :transform [[:set-value [:greeting ] set-value-transform]]
+   :emit [{:init init-main}
+          [#{[:* ]} (app/default-emitter [])]]
+   })
